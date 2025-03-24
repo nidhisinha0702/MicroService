24e57cd7a83f7503ae0523d5a5466800fa8986c3
Nidhi Kumari <nsinha7295@gmail.com>
2025-03-24 17:42:47 -0400

    openfeignclient for inter-comm

--- order-service/src/main/java/codewithnidhi/order_service/service/OrderService.java
+++ order-service/src/main/java/codewithnidhi/order_service/service/OrderService.java
@@ -3,18 +3,25 @@ package codewithnidhi.order_service.service;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import codewithnidhi.order_service.client.InventoryClient;
 import codewithnidhi.order_service.model.Order;
 import codewithnidhi.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
 	
 	private final OrderRepository orderRepository;
+	private final InventoryClient inventoryClient;
 
 	public void placeOrder(OrderRequest orderRequest) {
+		var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+		
+		if(isProductInStock) {
 		//map orderrequest to order object
 		Order order = new Order();
 		order.setOrderNumber(UUID.randomUUID().toString());
@@ -23,5 +30,8 @@ public class OrderService {
 		order.setQuantity(orderRequest.quantity());
 		//save order to orderrequest
 		orderRepository.save(order);
+		}else {
+			throw new RuntimeException("Product with skuCode "+ orderRequest.skuCode()+ " is not in stock");
+		}
 	}
 }
--- order-service/src/test/java/codewithnidhi/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/codewithnidhi/order_service/OrderServiceApplicationTests.java
@@ -1,17 +1,28 @@
 package codewithnidhi.order_service;
 
-import io.restassured.RestAssured;
-
 import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.testcontainers.containers.MySQLContainer;
 
+import codewithnidhi.order_service.stubs.InventoryClientStub;
+import io.restassured.RestAssured;
+import static org.awaitility.Awaitility.await;
+
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
 	@ServiceConnection
@@ -20,10 +31,22 @@ class OrderServiceApplicationTests {
 	@LocalServerPort
 	private Integer port;
 	
+	@Value("${wiremock.server.port}")
+	private int wireMockPort;
+	
 	@BeforeEach
 	void setUp() {
 		RestAssured.baseURI = "http://localhost";
 		RestAssured.port = port;
+		// Ensure WireMock is ready before continuing
+	    await().atMost(5, TimeUnit.SECONDS).until(() -> {
+	        try {
+	            new URL("http://localhost:" + wireMockPort + "/__admin/").openConnection().connect();
+	            return true;
+	        } catch (IOException e) {
+	            return false;
+	        }
+	    });
 	}
 	
 	static {
@@ -39,6 +62,7 @@ class OrderServiceApplicationTests {
 			    "quantity":1
 				}
 				""";
+		InventoryClientStub.stubInventoryCall("iphone_15", 1);
 		
 		var responseBodyString = RestAssured.given()
 				.contentType("application/json")
