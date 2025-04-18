2d948b38a8c12bddc19e93ec540b6299888df3ad
Nidhi Kumari <nsinha7295@gmail.com>
2025-04-18 15:30:59 -0400

    kafka mq

--- order-service/src/main/java/codewithnidhi/order_service/service/OrderService.java
+++ order-service/src/main/java/codewithnidhi/order_service/service/OrderService.java
@@ -2,10 +2,14 @@ package codewithnidhi.order_service.service;
 
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import codewithnidhi.order_service.client.InventoryClient;
+import codewithnidhi.order_service.event.OrderPlacedEvent;
 import codewithnidhi.order_service.model.Order;
 import codewithnidhi.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,8 +19,10 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 public class OrderService {
 	
+	private static final Logger log = LoggerFactory.getLogger(OrderService.class);
 	private final OrderRepository orderRepository;
 	private final InventoryClient inventoryClient;
+	private final KafkaTemplate<String,OrderPlacedEvent> kafkaTemplate;
 
 	public void placeOrder(OrderRequest orderRequest) {
 		var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
@@ -28,8 +34,18 @@ public class OrderService {
 		order.setPrice(orderRequest.price());
 		order.setSkuCode(orderRequest.skuCode());
 		order.setQuantity(orderRequest.quantity());
-		//save order to orderrequest
+		//save order to order request
 		orderRepository.save(order);
+		//send the message to kafka topic
+		OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+		orderPlacedEvent.setOrderNumber(order.getOrderNumber());
+		orderPlacedEvent.setEmail(orderRequest.userDetails().email());
+		orderPlacedEvent.setFirstName(orderRequest.userDetails().firstName());
+		orderPlacedEvent.setLastName(orderRequest.userDetails().lastName());
+		//orderNumber, email
+		log.info("start-sending orderplaced event {} to kafka topic", orderPlacedEvent);
+		kafkaTemplate.send("order-placed", orderPlacedEvent);
+		log.info("end-sending orderplaced event {} to kafka topic", orderPlacedEvent);
 		}else {
 			throw new RuntimeException("Product with skuCode "+ orderRequest.skuCode()+ " is not in stock");
 		}
